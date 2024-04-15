78f004a68ea2c9ed289cd749fc91bb2769dd189d
olegsmushko <olegsmushko@gmail.com>
2024-04-15 14:50:34 +0300

    Remove paused condition in reader function

--- main.cpp
+++ main.cpp
@@ -11,17 +11,14 @@
 #define READ  0
 #define WRITE 1
 
-void reader(int readPipe, std::unique_ptr<Logger> &logger, EventTracker &tracker, std::atomic<bool> &running,
-            std::atomic<bool> &paused) {
+void reader(int readPipe, std::unique_ptr<Logger> &logger, EventTracker &tracker, std::atomic<bool> &running) {
   while (running) {
-    if (!paused) {
-      Event e{};
-      if (read(readPipe, &e, sizeof(Event)) <= 0) {
-        std::cerr << "Error reading from pipe or pipe closed. " << std::strerror(errno) << std::endl;
-      }
-      logger->Write(e);
-      tracker.Increment();
+    Event e{};
+    if (read(readPipe, &e, sizeof(Event)) <= 0) {
+      std::cerr << "Error reading from pipe or pipe closed. " << std::strerror(errno) << std::endl;
     }
+    logger->Write(e);
+    tracker.Increment();
   }
 }
 
@@ -56,8 +53,8 @@ void writer(int writePipe, std::atomic<int> &minEventInterval, std::atomic<int> 
 int main() {
   std::atomic<bool> running(true);
   std::atomic<bool> paused(false);
-  std::atomic<int> minEventInterval(1000);
-  std::atomic<int> maxEventInterval(3000);
+  std::atomic<int> minEventInterval(500);
+  std::atomic<int> maxEventInterval(2000);
 
   auto logger = Logger::GetLogger(2);
   EventTracker tracker;
@@ -74,7 +71,7 @@ int main() {
 
   std::thread tWriter(writer, fd[WRITE], std::ref(minEventInterval), std::ref(maxEventInterval), std::ref(running),
                       std::ref(paused));
-  std::thread tReader(reader, fd[READ], std::ref(logger), std::ref(tracker), std::ref(running), std::ref(paused));
+  std::thread tReader(reader, fd[READ], std::ref(logger), std::ref(tracker), std::ref(running));
 
   commandHandler.printCommands();
   std::string input;
