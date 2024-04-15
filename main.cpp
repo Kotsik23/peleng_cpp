4d1f8624864c51921a3715279fb9f51e85cb69dd
olegsmushko <olegsmushko@gmail.com>
2024-04-15 15:44:37 +0300

    Non-blocking reader and writer fix

--- main.cpp
+++ main.cpp
@@ -11,14 +11,35 @@
 #define READ  0
 #define WRITE 1
 
+
+#include <fcntl.h>
+
+int setNonBlocking(int fd) {
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (flags == -1) return -1;
+  return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
 void reader(int readPipe, std::unique_ptr<Logger> &logger, EventTracker &tracker, std::atomic<bool> &running) {
+  Event e{};
   while (running) {
-    Event e{};
-    if (read(readPipe, &e, sizeof(Event)) <= 0) {
-      std::cerr << "Error reading from pipe or pipe closed. " << std::strerror(errno) << std::endl;
+    ssize_t bytesRead = read(readPipe, &e, sizeof(Event));
+    if (bytesRead > 0) {
+      logger->Write(e);
+      tracker.Increment();
+    } else if (bytesRead == 0) {
+      std::cerr << "Pipe closed." << std::endl;
+      break;  // Pipe was closed
+    } else {
+      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(100));
+        continue;
+      } else {
+        std::cerr << "Error reading from pipe: " << std::strerror(errno) << std::endl;
+        break;
+      }
     }
-    logger->Write(e);
-    tracker.Increment();
   }
 }
 
@@ -40,8 +61,14 @@ void writer(int writePipe, std::atomic<int> &minEventInterval, std::atomic<int> 
       e.param2 = dist(gen);
       e.param3 = dist(gen);
 
-      if (write(writePipe, &e, sizeof(Event)) == -1) {
-        std::cerr << "Error writing to pipe. " << strerror(errno) << std::endl;
+      ssize_t bytesWritten = write(writePipe, &e, sizeof(Event));
+      if (bytesWritten == -1) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+          std::this_thread::sleep_for(std::chrono::milliseconds(100));
+          continue;
+        } else {
+          std::cerr << "Error writing to pipe: " << strerror(errno) << std::endl;
+        }
       }
     }
     std::uniform_int_distribution<> sleepDist(minEventInterval, maxEventInterval);
@@ -69,6 +96,9 @@ int main() {
     return 1;
   }
 
+  setNonBlocking(fd[READ]);
+  setNonBlocking(fd[WRITE]);
+
   std::thread tWriter(writer, fd[WRITE], std::ref(minEventInterval), std::ref(maxEventInterval), std::ref(running),
                       std::ref(paused));
   std::thread tReader(reader, fd[READ], std::ref(logger), std::ref(tracker), std::ref(running));
@@ -82,8 +112,11 @@ int main() {
   }
 
 
+  std::cout << "Waiting for threads to finish..." << std::endl;
   tWriter.join();
   tReader.join();
+
+  std::cout << "All threads have finished. Exiting program." << std::endl;
 
   close(fd[READ]);
   close(fd[WRITE]);
